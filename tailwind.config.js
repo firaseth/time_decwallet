035a3ac4134379d60794dbbb7316a075e3c9393a
Firas.Eth <118602027+firaseth@users.noreply.github.com>
2026-01-21 18:54:01 +0300

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+    content: [
+        "./index.html",
+        "./components/**/*.{js,ts,jsx,tsx}",
+        "./src/**/*.{js,ts,jsx,tsx}",
+    ],
+    theme: {
+        extend: {
+            colors: {
+                slate: {
+                    700: '#334155',
+                    800: '#1e293b',
+                    900: '#0f172a',
+                }
+            }
+        },
+    },
+    plugins: [],
+}
